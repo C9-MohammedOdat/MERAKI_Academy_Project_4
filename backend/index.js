13868f2ed105e104e41600679022ea5e1a1a644c
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-04 10:53:20 -0800

    Add roles

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,11 @@ require("./models/db");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const usersRouter =require("./routes/users")
+const rolesrouter =require("./routes/roles")
 app.use(cors());
 app.use(express.json());
 app.use("/users",usersRouter)
+app.use("/roles",rolesrouter)
 // Handles any other endpoints [unassigned - endpoints]
 app.use("*", (req, res) => res.status(404).json("NO content at this path"));
 
