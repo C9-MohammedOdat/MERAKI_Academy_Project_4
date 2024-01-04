a9da11c23e6aa184bc57fae5213e0a277db449e5
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-04 09:32:52 -0800

    Create Category Schema

---
+++ backend/models/categorySchema.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const category =new mongoose.Schema({
+    title:{type : String, required:true},
+price: {type :Number ,required:true},
+provider:{type :mongoose.Schema.Types.ObjectId,ref:"User"},
+})
+module.exports=mongoose.model(category,"Category")
