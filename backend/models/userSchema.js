6f913a3c71742f426f294c5b5f62d7765df5a123
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-04 09:16:15 -0800

    User Schema

---
+++ backend/models/userSchema.js
@@ -0,0 +1,12 @@
+import mongoose, { model } from "mongoose";
+const user = new mongoose.Schema({
+    firstName:{type:String, required:true},
+lastName: {type:String} ,
+phoneNumber:{type:Number, required:true},
+location:[{type:Number, required:true}], // [longitude, latitude]
+email:{ type:String, unique:true, required:true},
+password: {type:String, required:true},
+category:{ type: mongoose.Schema.Types.ObjectId, ref: "Category"},
+image:{type:String},
+})
+module.exports=mongoose.model("User",user)
