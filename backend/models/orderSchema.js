59291c5d2ab3dad9d03993dcc673c9efc0a0f226
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-04 09:38:31 -0800

    Create order Schema

---
+++ backend/models/orderSchema.js
@@ -0,0 +1,7 @@
+import mongoose from "mongoose";
+const order = new mongoose.Schema({
+    client:{type:mongoose.Schema.Types.ObjectId,ref:"User"},
+    state:{type:String},
+    provider:{type:mongoose.Schema.Types.ObjectId,ref:"User"}
+})
+module.exports=mongoose.model(order,"Order")
