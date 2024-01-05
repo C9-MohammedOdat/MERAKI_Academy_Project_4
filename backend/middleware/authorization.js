3d42c406cd5033e8c82a38006b1666df478aa5ac
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-05 07:27:57 -0800

    Autorization

---
+++ backend/middleware/authorization.js
@@ -0,0 +1,16 @@
+const authorization = (string) => {
+  
+    return (req, res, next) => {
+      console.log(req.token.role);
+      if (!req.token.role.permissions.includes(string)) {
+        return res.status(403).json({
+          success: false,
+          message: `Unauthorized`,
+        });
+      }
+      next();
+    };
+  };
+  
+  module.exports = authorization;
+  
