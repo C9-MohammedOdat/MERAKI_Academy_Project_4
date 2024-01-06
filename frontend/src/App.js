d3134826fae5bf2f8d219e8266d29e168918df35
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-06 14:38:42 -0800

    Login in Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
 import "./App.css";
-
+import { Routes, Route, Link } from "react-router-dom";
+import Login from "./components/Login/Login";
+import React, { createContext, useState } from "react";
+export const LoginContext = createContext();
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(localStorage.getItem("isLoggedIn")||false);
+  const [token, setToken] = useState(localStorage.getItem("token") || "");
   return (
-    <div className="App">
-      <h1>Hello, World!</h1>
-    </div>
+    <LoginContext.Provider value={{setIsLoggedIn,setToken,token,isLoggedIn}}>
+      <Routes>
+        <Route path="login" element={<Login />} />
+      </Routes>
+    </LoginContext.Provider>
   );
 }
 
---
+++ frontend/src/components/Login/Login.js
@@ -0,0 +1,39 @@
+import "./Login.css"
+import React ,{useState,useContext} from 'react'
+import axios from 'axios'
+import { LoginContext } from "../../App"
+const Login = () => {
+    const{setIsLoggedIn,setToken,token,isLoggedIn}=useContext(LoginContext)
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [resFromBack, setResFromBack] = useState("")
+    const sendLogin=()=>{
+    axios.post("http://localhost:5000/users/login",{email,password}).then((result)=>{
+        console.log(result);
+        setResFromBack(result.data)
+        setIsLoggedIn(true)
+        setToken(result.data.token)
+        localStorage.setItem("token",result.data.token)
+        localStorage.setItem("isLoggedIn",true)
+    }).catch((err)=>{
+        console.log(err);
+        setResFromBack(err.response.data)
+    })
+    }
+    
+  return (
+    <div className='Login-Page'>
+        <div><h1>Login</h1></div>
+        <div><input onChange={(e)=>{
+            setEmail(e.target.value)
+        }} type='email' placeholder='Email'/></div>
+        <div><input onChange={(e)=>{
+            setPassword(e.target.value)
+        }} type='password' placeholder='Password'/></div>
+        <div><button onClick={sendLogin}>Login</button></div>
+        {resFromBack&&<div className={resFromBack.success?"succ":"err"}>{resFromBack.message}</div>}
+        </div>
+  )
+}
+
+export default Login
