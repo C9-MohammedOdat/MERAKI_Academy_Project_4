53970362510ee47e81fb71e3d35c650ff007cefd
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-10 03:56:28 -0800

    Login Editing Completed

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -25,14 +25,18 @@ const Login = () => {
     
   return (
     <div className='Login-Page'>
-        <div><h1>Login</h1></div>
+         <div className='userLogo'><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-circle" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"/>
+</svg></div>
+    <div className='Welcome'><h1>Welcome Back !</h1></div>
         <div><input onChange={(e)=>{
             setEmail(e.target.value)
         }} type='email' placeholder='Email'/></div>
         <div><input onChange={(e)=>{
             setPassword(e.target.value)
         }} type='password' placeholder='Password'/></div>
-        <div><button onClick={sendLogin}>Login</button></div>
+        <div><button className="Login-btn" onClick={sendLogin}>Login</button></div>
         {resFromBack&&<div className={resFromBack.success?"succ":"err"}>{resFromBack.message}</div>}
         </div>
   )
