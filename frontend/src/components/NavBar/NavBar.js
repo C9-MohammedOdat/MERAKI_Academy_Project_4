59fd92f864fafe5e6fb378afd7cd4c31387585aa
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-14 13:54:50 -0800

    Some Changes

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -8,8 +8,9 @@ import Button from 'react-bootstrap/Button';
 import Offcanvas from 'react-bootstrap/Offcanvas';
 import "./NavBar.css"
 import { LoginContext } from '../../App';
+import axios from 'axios';
 const NavBar = () => {
-  const {isLoggedIn,role,notification}=useContext(LoginContext)
+  const {isLoggedIn,role,notification,token}=useContext(LoginContext)
     const [show, setShow] = useState(false);
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
@@ -17,12 +18,39 @@ const NavBar = () => {
     const handleClose1 = () => setShow1(false);
     const handleShow1 = () => setShow1(true);
   const navigate=useNavigate()
-    const filteredNotification=notification.filter((ele,i)=>ele.state==="pending"&&ele.price)
+    let filteredNotification=notification.filter((ele,i)=>ele.state==="pending"&&ele.price)
     console.log(filteredNotification);
     const logout=()=>{
       setShow(false)
       localStorage.clear()
       navigate("/login")
+    }
+    const rejectOrder=(id)=>{
+      const newNotification=filteredNotification.filter((ele,i)=>{
+        return ele._id!=id
+      })
+      filteredNotification=newNotification
+      axios.delete(`http://localhost:5000/orders/${id}`).then((result)=>{
+        console.log(result)
+      }).catch((err)=>{
+        console.log(err);
+      })
+    }
+    const updateOrder=(id)=>{
+      const newNotification=filteredNotification.map((ele,i)=>{
+       if (ele._id!=id){
+        ele.state="processing"
+        return ele
+       }
+      })
+      filteredNotification=newNotification
+
+      axios.put(`http://localhost:5000/orders/${id}`,{state:"processing"},{headers:{authorization:`Bearer ${token}`,
+    }}).then((result)=>{
+        console.log(result)
+      }).catch((err)=>{
+        console.log(err);
+      })
     }
   return (
     <Navbar style={{paddingLeft:"5px",paddingRight:"10px"}} bg="dark" data-bs-theme="dark">
@@ -54,25 +82,34 @@ const NavBar = () => {
       </Nav>
     </Container>
 {isLoggedIn&&<div className='notification'>
-<svg 
+  {filteredNotification.length!==0?<svg 
  onClick={handleShow1}
-xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-bell" viewBox="0 0 16 16" className='notification' style={filteredNotification&&{fill:"red"}}>
+xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="red" class="bi bi-bell" viewBox="0 0 16 16" className='notification'>
   <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
-</svg>
+</svg>:<svg 
+ onClick={handleShow1}
+xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="white" class="bi bi-bell" viewBox="0 0 16 16" className='notification'>
+  <path d="M8 16a2 2 0 0 0 2-2H6a2 2 0 0 0 2 2M8 1.918l-.797.161A4 4 0 0 0 4 6c0 .628-.134 2.197-.459 3.742-.16.767-.376 1.566-.663 2.258h10.244c-.287-.692-.502-1.49-.663-2.258C12.134 8.197 12 6.628 12 6a4 4 0 0 0-3.203-3.92zM14.22 12c.223.447.481.801.78 1H1c.299-.199.557-.553.78-1C2.68 10.2 3 6.88 3 6c0-2.42 1.72-4.44 4.005-4.901a1 1 0 1 1 1.99 0A5 5 0 0 1 13 6c0 .88.32 4.2 1.22 6"/>
+</svg>}
+
 
       <Offcanvas show={show1} onHide={handleClose1} placement='end'>
         <Offcanvas.Header closeButton>
           <Offcanvas.Title>Notification</Offcanvas.Title>
         </Offcanvas.Header>
         <Offcanvas.Body style={{display:"flex", flexDirection:"column"}}>
-          {filteredNotification.length&&filteredNotification.map((ele,i)=>
+          {filteredNotification.length!==0&&filteredNotification.map((ele,i)=>
             <div style={{border:"1px solid gray",padding:"5px",borderRadius:"7px", display:"flex",flexDirection:"column",gap:"10px"}}>
           <div><h6>{ele.provider.firstName}</h6></div>
          {ele.units&&<div>Number Of Cylenders : {ele.units} Cyl</div>} 
           <div>Total Price :{ele.price} JD</div>
           <div style={{display:"flex",justifyContent:"flex-end",gap:"5px"}}>
-            <Button  variant='primary' >Accept</Button>
-            <Button variant='danger' >reject</Button>
+            <Button  variant='primary' onClick={()=>{
+              updateOrder(ele._id)
+            }} >Accept</Button>
+            <Button variant='danger' onClick={()=>{
+              rejectOrder(ele._id)
+            }} >reject</Button>
           </div>
             </div>
           )}
