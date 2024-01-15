e0c2f8c142e916854e5f021defa883c4ebe3e093
MohammedOdat <Mohammed.A.Odatt@gmail.com>
2024-01-15 13:09:40 -0800

    Some Change

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React,{useState,useEffect,useContext,createContext} from 'react'
 import axios from 'axios'
 import Carousel from 'react-bootstrap/Carousel';
+import Card from 'react-bootstrap/Card';
 // import ExampleCarouselImage from '../images/Gas.jpg';
 import Button from 'react-bootstrap/Button';
 import { LoginContext } from '../../App'
@@ -71,7 +72,50 @@ const Home = () => {
         <Button onClick={()=>{
             navigate("register")
         }} variant="link" style={{color:"black",fontSize:"1.5em"}} >Register now</Button>
-
+<div className='cards'> 
+<Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title><h2>About Us</h2></Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-people-fill" viewBox="0 0 16 16">
+  <path d="M7 14s-1 0-1-1 1-4 5-4 5 3 5 4-1 1-1 1zm4-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6m-5.784 6A2.24 2.24 0 0 1 5 13c0-1.355.68-2.75 1.936-3.72A6.3 6.3 0 0 0 5 9c-4 0-5 3-5 4s1 1 1 1zM4.5 8a2.5 2.5 0 1 0 0-5 2.5 2.5 0 0 0 0 5"/>
+</svg></Card.Subtitle>
+        <Card.Text style={{fontSize:"1.5em"}}>
+        We Are Multi Delivery Services by Heavy Car ,
+         Our service is very fast with less cost.
+          order us now.. 
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title><h2>Contact Us</h2></Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/>
+</svg></Card.Subtitle>
+        <Card.Text style={{fontSize:"1.25em"}}>
+        <p> contact us by:</p>
+        <p style={{fontSize:"1em"}}> Phone Number : +962796042844</p>
+         <p  style={{fontSize:"1em"}}>Email Address : mohammed.a.odatt@gmail.com</p>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    <Card style={{ width: '18rem' }}>
+      <Card.Body>
+        <Card.Title><h2>Be With Us</h2></Card.Title>
+        <Card.Subtitle className="mb-2 text-muted"><svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-person-fill-check" viewBox="0 0 16 16">
+  <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m1.679-4.493-1.335 2.226a.75.75 0 0 1-1.174.144l-.774-.773a.5.5 0 0 1 .708-.708l.547.548 1.17-1.951a.5.5 0 1 1 .858.514M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
+  <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
+</svg></Card.Subtitle>
+        <Card.Text style={{fontSize:"1.5em"}}>
+       <p> It's free.</p> 
+        <p>No subscription required</p>
+        <Button onClick={()=>{
+            navigate("register")
+        }} variant="link" style={{color:"blue",fontSize:"1em",textDecoration:"none" }} >Register now</Button>
+        </Card.Text>
+      </Card.Body>
+    </Card>
+    </div>
         </div>
   )
 }
